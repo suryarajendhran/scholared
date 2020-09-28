2cb71bfc91c598745ce6a37215d4a7273da2d32e
Surya Rajendhran <cielograndesurya@gmail.com>
2020-09-28 16:16:48 +0530

    Submit button radius

--- pages/quiz.vue
+++ pages/quiz.vue
@@ -179,7 +179,7 @@ body {
   padding: 0.35em 1.2em;
   border: 0.1em solid #ffffff;
   margin: 0 0.3em 0.3em 0;
-  border-radius: 0.12em;
+  border-radius: 0.5em;
   box-sizing: border-box;
   text-decoration: none;
   cursor: pointer;

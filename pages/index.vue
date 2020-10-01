f84766bad6d84bb0d69f8929c030ce3d5204dbf5
Surya Rajendhran <cielograndesurya@gmail.com>
2020-10-01 16:01:25 +0530

    Index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <h1>This is content inside the index.vue file!</h1>
+  <section
+    id="indexSection"
+    @click="changeColor"
+    :class="{ section: true, white: !darkModeToggle, dark: darkModeToggle }"
+  >
+    <div class="container columns">
+      <nuxt-link :class="linkClass" to="/quiz">
+        <p class="title">Quiz Interface</p>
+        <p class="subtitle">The ❤️ of Scholared!</p>
+      </nuxt-link>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -7,15 +18,37 @@ export default {
   head: {
     title: 'Home page 🚀',
     noscript: [{ innerHTML: 'Body No Scripts', body: true }],
-    script: [
-      { src: '/head.js' },
-      // Supported since 1.0
-      { src: '/body.js', body: true },
-      { src: '/defer.js', defer: '' },
-    ],
+  },
+  data: function () {
+    return {
+      linkClass: {
+        notification: true,
+        'is-warning': true,
+        column: true,
+        'is-two-fifths': true,
+        'has-text-centered': true,
+      },
+      darkModeToggle: false,
+    }
+  },
+  methods: {
+    changeColor: function () {
+      this.darkModeToggle = !this.darkModeToggle
+    },
   },
 }
 </script>
 
-<style>
+<style scoped>
+#indexSection {
+  height: 100vh;
+}
+
+.white {
+  background-color: white;
+}
+
+.dark {
+  background-color: rgb(24, 23, 23);
+}
 </style>

4643909cae9b1605b79a43169406dcb9ed600a8e
Surya Rajendhran <cielograndesurya@gmail.com>
2020-10-06 01:39:59 +0530

    Messy code man

--- components/ResultsModal.vue
+++ components/ResultsModal.vue
@@ -3,6 +3,25 @@
     <div class="modal-background"></div>
     <div class="modal-content">
       <div class="box">
+        <h1 class="is-size-1 has-text-centered">Results</h1>
+        <p class="my-2 is-size-5">
+          Percentage of correct answers:
+          <span class="is-pulled-right has-text-primary"
+            >{{ correctPercentage }}%</span
+          >
+        </p>
+        <p class="my-2 is-size-5">
+          Time taken to respond to questions:
+          <span class="is-pulled-right has-text-primary">{{
+            formattedAvgResponseTime
+          }}</span>
+        </p>
+        <p class="my-2 is-size-5">
+          Number of choice changes:
+          <span class="is-pulled-right has-text-primary">{{
+            formattedChoiceChanges
+          }}</span>
+        </p>
         <button class="button is-white" @click="printResults">
           View results array
         </button>
@@ -25,8 +44,6 @@ export default {
   name: 'ResultsModal',
   data() {
     return {
-      avgResponseTime: null,
-      avgChoiceChanges: null,
       toggleClose: false,
     }
   },
@@ -43,17 +60,42 @@ export default {
     printResults: function () {
       console.log(this.results)
     },
+    formatFloat: function (floatValue) {
+      return `${Math.round(floatValue * 100) / 100}`
+    },
   },
-  watch: {
-    results: function (val) {
-      if (val) {
-        const timeData = this.results.answerData.map(
-          ({ timeTaken }) => timeTaken
-        )
-        this.avgResponseTime =
-          timeData.reduce((a, b) => a + b, 0) / timeData.length
-        console.log(this.avgResponseTime)
+  computed: {
+    correctPercentage: function () {
+      if (this.results) {
+        return (this.results.correctAnswers / this.results.totalQuestions) * 100
       }
+      return 0
+    },
+    formattedAvgResponseTime: function () {
+      if (!this.results) {
+        return 0
+      }
+
+      const timeData = this.results.answerData.map(({ timeTaken }) => timeTaken)
+
+      const avgResponseTime =
+        timeData.reduce((a, b) => a + b, 0) / timeData.length
+
+      return `${this.formatFloat(avgResponseTime)} secs.`
+    },
+    formattedChoiceChanges: function () {
+      if (!this.results) {
+        return 0
+      }
+
+      const choiceData = this.results.answerData.map(
+        ({ choiceChanges }) => choiceChanges
+      )
+
+      const avgChoices =
+        choiceData.reduce((a, b) => a + b, 0) / choiceData.length
+
+      return `${this.formatFloat(avgChoices)}`
     },
   },
 }
